5291c917d3f961779853a91de13f4d6509a31173
Espen <144965457+Standal2@users.noreply.github.com>
2025-08-08 13:23:34 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [msg, setMsg] = useState("");
+
+  useEffect(() => {
+    fetch("/api/hello")
+      .then((res) => res.json())
+      .then((data) => setMsg(data.message))
+      .catch(console.error);
+  }, []);
   const value = 'World';
   return <div>Hello {value}</div>;
+  <p>{msg}</p>
 }
+
 
 export default App;
